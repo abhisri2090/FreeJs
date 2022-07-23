beb757e524a559977f9259c4eeb90d798a329767
abhisri2090 <31709161+abhisri2090@users.noreply.github.com>
2022-07-23 21:23:14 +0530

    added hook

---
+++ useApiCall/TsVersion/axios.schema.ts
@@ -0,0 +1,29 @@
+import { AxiosRequestConfig, CancelTokenSource, AxiosResponse } from 'axios';
+
+export interface RequestConfig extends AxiosRequestConfig {
+  options?: {
+    hasFormData?: boolean;
+    defaultLoader?: boolean;
+    fullResponse?: boolean;
+    hideDefaultError?: boolean;
+  };
+}
+
+export interface ResponseConfig {
+  options: {
+    defaultLoader?: boolean;
+    fullResponse?: boolean;
+    defaultError?: boolean;
+  };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface CustomAxios<T = any> {
+  axiosCall: Promise<AxiosResponse<T>>;
+  abort: () => void;
+}
+
+export type JsonFormData = Record<string, string | Blob>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ApiRequest = (token: CancelTokenSource['token']) => Promise<AxiosResponse<any>>;
